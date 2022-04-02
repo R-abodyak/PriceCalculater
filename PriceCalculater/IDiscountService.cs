48769bfcafc6fb92bb82d0512ea3aa3745b9138e
R-abodyak <rawanabodiak12@gmail.com>
2022-04-02 23:43:18 +0300

    DISCOUNT : add discount service implemntasion

---
+++ PriceCalculater/IDiscountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculater
+{
+    internal interface IDiscountService
+    {
+        public decimal GetDiscountPercentage();
+    }
+    public class DiscountService : IDiscountService
+    {
+        private readonly decimal _percentage;
+        public DiscountService(decimal percentage)
+        {
+            this._percentage = percentage;
+        }
+        public decimal GetDiscountPercentage()
+        {
+            return _percentage;
+        }
+    }
+}
