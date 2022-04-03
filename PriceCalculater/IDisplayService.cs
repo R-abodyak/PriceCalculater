87a5654eadd162a02cf141fe3af224716e1f5996
R-abodyak <rawanabodiak12@gmail.com>
2022-04-03 12:59:32 +0300

    REPORT :modify display in consoledisplay class

--- PriceCalculater/IDisplayService.cs
+++ PriceCalculater/IDisplayService.cs
@@ -5,12 +5,8 @@
 public class ConsoleDisplayService : IDisplayService
 {
     private string formatString = "{0,0:" + ".00" + "}";
-  
     public void Display(decimal value)
-    {
+    {   if (value == 0) return;
         Console.WriteLine(formatString, value);
     }
-
 }
-
-
