8fd362a6084f9bb2701d450018dff38a6497bd37
R-abodyak <rawanabodiak12@gmail.com>
2022-04-03 14:20:27 +0300

    SELECTIVE : implement upcdiscountservice Class

--- PriceCalculater/IDiscountService.cs
+++ PriceCalculater/IDiscountService.cs
@@ -21,4 +21,19 @@ namespace PriceCalculater
             return _percentage;
         }
     }
+    public class UpcDiscountService : IDiscountService
+    {
+        private readonly Dictionary<long,decimal> _UpcDiscountList;
+        private readonly long _upc;
+        public UpcDiscountService(Dictionary<long, decimal> UpcDiscountList,long upc)
+        {
+            this._UpcDiscountList = UpcDiscountList;
+            this._upc = upc;
+        }
+        public decimal GetDiscountPercentage()
+        {
+            if (!_UpcDiscountList.ContainsKey(_upc)) return 0;
+            return _UpcDiscountList[_upc];
+        }
+    }
 }
