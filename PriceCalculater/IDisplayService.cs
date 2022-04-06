fd82ffde0754f3924c33bdb2f285745b34e1c4ba
R-abodyak <rawanabodiak12@gmail.com>
2022-04-06 22:35:30 +0300

    Currency : modify display method to print currency

--- PriceCalculater/IDisplayService.cs
+++ PriceCalculater/IDisplayService.cs
@@ -1,13 +1,14 @@
 ﻿public interface IDisplayService
 {
-    public void Display(String msg,decimal value);
+    public void Display(String msg,decimal value,String currency);
 }
 public class ConsoleDisplayService : IDisplayService
 {
     private string formatString = "{0,0:" + ".00" + "}";
-    public void Display(String msg, decimal value)
+    public void Display(String msg, decimal value ,String currency)
     {   if (value == 0) return;
         Console.Write(msg);
-        Console.WriteLine(formatString, value);
+        Console.Write(formatString, value);
+        Console.WriteLine("  " +currency);
     }
 }
