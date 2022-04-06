69a1e066fdcb75f7e75bb4d75d45229c27a98656
R-abodyak <rawanabodiak12@gmail.com>
2022-04-06 22:32:32 +0300

    CURRENCY : add currency prop to price

--- PriceCalculater/Product.cs
+++ PriceCalculater/Product.cs
@@ -3,4 +3,6 @@
     public String Name { get; set; } = "undefined name";
     public long UPC { get; set; }
     public decimal Price { get; set; }
+    public Iso_3 Currency { get; set; } = Iso_3.USD
 }
+public enum Iso_3 { USD ,GBP,JPY};
