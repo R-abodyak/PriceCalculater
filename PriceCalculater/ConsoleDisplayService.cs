683ec50a6803ca0878fe87a1cbbb9d6d0c595f60
R-abodyak <rawanabodiak12@gmail.com>
2022-04-25 09:50:30 +0300

    seperate interfaces and classes

---
+++ PriceCalculater/ConsoleDisplayService.cs
@@ -0,0 +1,11 @@
+﻿public class ConsoleDisplayService : IDisplayService
+{
+    private string formatString = "{0,0:" + ".00" + "}";
+  
+    public void Display(decimal value)
+    {
+        Console.WriteLine(formatString, value);
+    }
+}
+
+
