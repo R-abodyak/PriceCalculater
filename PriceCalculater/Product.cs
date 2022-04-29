124be9cdf04897622aba39dcd6d2566de024e7a1
R-abodyak <rawanabodiak12@gmail.com>
2022-04-29 14:00:09 +0300

    Currency: add currency attribute  to product

--- PriceCalculater/Product.cs
+++ PriceCalculater/Product.cs
@@ -4,4 +4,6 @@ public class Product
     public String Name { get; set; } = "undefined name";
     public long UPC { get; set; }
     public decimal Price { get; set; }
+    public Iso_3 Currency { get; set; } = Iso_3.USD;
 }
+public enum Iso_3 { USD, GBP, JPY };
