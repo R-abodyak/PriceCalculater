ed8e7647bb7a159b481848310cf8b3fa3ef8a6c8
R-abodyak <rawanabodiak12@gmail.com>
2022-04-29 14:40:18 +0300

    Currency : modify report test

--- PriceCalculater/ExtensionClass.cs
+++ PriceCalculater/ExtensionClass.cs
@@ -10,7 +10,7 @@ namespace PriceCalculater
     {
         public static decimal ApplyPrecision(this decimal price)
         {
-            return Math.Round(price, 2);
+            return Math.Round(price,2);
         }
     }
 }
