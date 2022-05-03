2d2080ea53f368781b3671c6ac704c70ebe4a94b
R-abodyak <rawanabodiak12@gmail.com>
2022-05-03 06:05:54 +0300

    Cap : add cap calcution in calculater

--- PriceCalculater/Calculater.cs
+++ PriceCalculater/Calculater.cs
@@ -28,18 +28,31 @@ public class Calculater
         decimal amount = (percentage / 100) * price;
         return amount.ApplyPrecision();
     }
-    public ProductPriceDetails FindProductDetails(Product product)
+    public ProductPriceDetails FindProductDetails(Product product, Cap cap = null)
     {
         var productPriceDetails = new ProductPriceDetails();
         var discountBeforeTax = CalculateDiscountBefore(product);
         decimal remaningPrice = product.Price - discountBeforeTax;
         productPriceDetails.TaxAmount = Calculate(remaningPrice, _taxService.GetTaxPercentage());
         var discountAfterTax = CalculateDiscountAfter(product, remaningPrice);
-        productPriceDetails.DiscountAmount = discountBeforeTax + discountAfterTax;
+
+        decimal totalDiscount = discountBeforeTax + discountAfterTax;
+        decimal CapCalculatedResult = totalDiscount;
+        CapCalculatedResult = CalculateCap(product, cap, CapCalculatedResult);
+        productPriceDetails.DiscountAmount = Math.Min(totalDiscount, CapCalculatedResult);
         var costList = _costService.GetCosts(product);
         FindCostDetails(productPriceDetails, product, costList);
         productPriceDetails.FinalPrice = (product.Price + productPriceDetails.TaxAmount - productPriceDetails.DiscountAmount + productPriceDetails.TotalCostAmount).ApplyPrecision();
         return productPriceDetails;
+    }
+    private decimal CalculateCap(Product product, Cap cap, decimal CapCalculatedResult)
+    {
+        if (cap != null)
+        {
+            if (cap._capType == AmountType.relative) CapCalculatedResult = cap._amountValue;
+            else CapCalculatedResult = Calculate(product.Price, cap._amountValue);
+        }
+        return CapCalculatedResult;
     }
 
     private void FindCostDetails(ProductPriceDetails productPriceDetails, Product product, List<Cost>? _costList)
